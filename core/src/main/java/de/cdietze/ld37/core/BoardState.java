5462a09626ce3aea29e0e296f2a0c8d62bfe83ae
Christoph Dietze <git@cdietze.de>
2016-12-10 10:17:16 +0100

    Add BoardScreen - fields are actually rendered

--- core/src/main/java/de/cdietze/ld37/core/BoardState.java
+++ core/src/main/java/de/cdietze/ld37/core/BoardState.java
@@ -7,6 +7,8 @@ import tripleplay.util.Logger;
 public class BoardState {
     public static final Logger log = new Logger("state");
 
-    public final Dimension dim = new Dimension(8,8);
+    public final Dimension dim = new Dimension(8, 8);
+
+    public final int fieldCount = dim.width * dim.height;
     public final RList<Entity> entities = RList.create();
 }
