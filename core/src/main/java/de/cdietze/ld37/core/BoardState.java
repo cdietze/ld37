e907e99a233542c5aaeabd997fcce056a8dbe0f7
Christoph Dietze <git@cdietze.de>
2016-12-10 09:41:38 +0100

    Add basic BoardState

---
+++ core/src/main/java/de/cdietze/ld37/core/BoardState.java
@@ -0,0 +1,12 @@
+package de.cdietze.ld37.core;
+
+import pythagoras.i.Dimension;
+import react.RList;
+import tripleplay.util.Logger;
+
+public class BoardState {
+    public static final Logger log = new Logger("state");
+
+    public final Dimension dim = new Dimension(8,8);
+    public final RList<Entity> entities = RList.create();
+}
