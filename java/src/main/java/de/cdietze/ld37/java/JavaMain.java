bb7d5dce3420d4ae04862bb8982d6ce132ec5971
Christoph Dietze <cdi@adorsys.de>
2016-12-10 16:45:42 +0100

    Bigger java screen

--- java/src/main/java/de/cdietze/ld37/java/JavaMain.java
+++ java/src/main/java/de/cdietze/ld37/java/JavaMain.java
@@ -1,15 +1,17 @@
 package de.cdietze.ld37.java;
 
-import playn.java.LWJGLPlatform;
-
 import de.cdietze.ld37.core.MainGame;
+import playn.java.LWJGLPlatform;
 
 public class JavaMain {
 
-  public static void main (String[] args) {
+  public static void main(String[] args) {
     LWJGLPlatform.Config config = new LWJGLPlatform.Config();
-    config.width = 800;
-    config.height= 450;
+    // 800x450 is the ludumdate.com embedded window size
+    // config.width = 800;
+    // config.height = 450;
+    config.width = 1280;
+    config.height = 1024;
     // use config to customize the Java platform, if needed
     LWJGLPlatform plat = new LWJGLPlatform(config);
     new MainGame(plat);
