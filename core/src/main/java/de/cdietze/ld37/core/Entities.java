299f2227b957294650a83147f635496f203a6e32
Christoph Dietze <git@cdietze.de>
2016-12-10 15:01:41 +0100

    Draw a mouse

--- core/src/main/java/de/cdietze/ld37/core/Entities.java
+++ core/src/main/java/de/cdietze/ld37/core/Entities.java
@@ -5,4 +5,8 @@ public class Entities {
   public static Entity createCat(int fieldIndex) {
     return new Entity(Entity.Type.CAT, fieldIndex) {};
   }
+
+  public static Entity createMouse(int fieldIndex) {
+    return new Entity(Entity.Type.MOUSE, fieldIndex) {};
+  }
 }
